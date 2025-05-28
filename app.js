6b3380a800463379b42e5cd0f21e65e6126a2b82
prof. Marnei Cardoso <marneicardoso.geracaotech@gmail.com>
2025-05-28 15:54:59 -0300

    update/cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
-import logger from "./middleware/logger.js"
+import logger from "./middleware/logger.js";
+import cors from "cors";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(logger);
+app.use(cors()); // Habilita CORS para todos os domínios
 
 // Rotas
 app.use("/api/users", userRoutes);
